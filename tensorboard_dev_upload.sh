36fa20810336ed4684b363c80a8f7877e9a1df9b
Leon Kim <serialexperimentsleon@gmail.com>
2020-12-09 15:39:17 -0500

    add convenience shell script for uploading multiple tensorboard logs

---
+++ tensorboard_dev_upload.sh
@@ -0,0 +1,12 @@
+# tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/02\:10\:09.892064/ --name "NoGravityMinBaselineObsNorm" &
+# tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/02\:10\:11.396287/ --name "NoGravityMinBaseline" &
+# tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/02\:10\:11.506064/ --name "NoGravityBaseline" 
+
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/16\:33\:18.478940/ --name "NoGravityBaselineObsNorm" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/16\:33\:18.478782/ --name "GravityBaseline" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/16\:33\:18.478725/ --name "ID00" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/21\:09\:10.537792/ --name "ID01" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/21\:09\:09.162569/ --name "ID02" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/21\:09\:10.537484/ --name "ID03" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-08/21\:09\:09.162602/ --name "ID04" &
+tensorboard dev upload --logdir /home/leonmkim/logs/stable/2020-12-09/01\:11\:56.796066/ --name "GravityBaselineNoTipVel" 
