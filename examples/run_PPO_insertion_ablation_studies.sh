5b5219fed36114b1384a7e243d41a4b0f6eba331
Leon Kim <serialexperimentsleon@gmail.com>
2020-12-09 17:01:29 -0500

    add obs normalized runs

--- examples/run_PPO_insertion_ablation_studies.sh
+++ examples/run_PPO_insertion_ablation_studies.sh
@@ -1,7 +1,8 @@
 # python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/GravityBaselineImpedanceV2PegInsertion:PPO2.json --final &
 # python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/GravityBaselineImpedanceV2PegInsertion:PPO2.json  &
 # python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/id0ImpedanceV2PegInsertion:PPO2.json &
-python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/id01ImpedanceV2PegInsertion:PPO2.json &
-python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/id02ImpedanceV2PegInsertion:PPO2.json &
-python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/id03ImpedanceV2PegInsertion:PPO2.json 
+python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/cluster/normalized/id1ImpedanceV2PegInsertion:PPO2.json &
+python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/cluster/normalized/id2ImpedanceV2PegInsertion:PPO2.json &
+python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/cluster/normalized/id3ImpedanceV2PegInsertion:PPO2.json 
+python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/cluster/normalized/id4ImpedanceV2PegInsertion:PPO2.json 
 # python3 modified_learn_environment.py --param_file=ablation_studies/peg_insertion/id4ImpedanceV2PegInsertion:PPO2.json --final
